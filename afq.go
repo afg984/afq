649230c54b9ee1263c9a8641929f8f481391a291
afg <afg984@gmail.com>
2017-04-09 18:31:48 +0800

    working w/ multiple machines

---
+++ afq.go
@@ -0,0 +1,417 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var port int
+var nodename string
+var nodetimeout time.Duration
+
+func init() {
+	flag.IntVar(&port, "port", 6666, "the port the server is supposed to be running on")
+	flag.DurationVar(&nodetimeout, "nodetimeout", time.Second*10, "time waiting for node to start")
+	var err error
+	nodename, err = os.Hostname()
+	if err != nil {
+		log.Fatalln("fatal:", err)
+	}
+}
+
+func split2(s string, by string, left interface{}, right interface{}) error {
+	splitresult := strings.SplitN(s, by, 2)
+	if len(splitresult) != 2 {
+		return errors.New("string does not match format")
+	}
+	_, err := fmt.Sscan(splitresult[0], left)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Sscan(splitresult[1], right)
+	return err
+}
+
+type trackedProcess struct {
+	cmd        *exec.Cmd
+	exited     bool
+	exitStatus int
+	waitExited chan bool
+}
+
+func newTrackedProcess(startedCommand *exec.Cmd) *trackedProcess {
+	tp := &trackedProcess{
+		cmd:        startedCommand,
+		exited:     false,
+		exitStatus: -1,
+		waitExited: make(chan bool),
+	}
+	go func() {
+		err := startedCommand.Wait()
+		tp.exited = true
+		close(tp.waitExited)
+		if err == nil {
+			tp.exitStatus = 0
+			log.Println("process", tp.cmd.Process.Pid, "exited successfully")
+		} else {
+			if exiterr, ok := err.(*exec.ExitError); ok {
+				if waitStatus, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+					tp.exitStatus = waitStatus.ExitStatus()
+					log.Println("process", tp.cmd.Process.Pid, "exited with status", tp.exitStatus)
+					return
+				}
+			}
+			log.Fatalf("unexpected error for %v, %v", startedCommand, err)
+		}
+	}()
+	return tp
+}
+
+type NodeWorker struct {
+	name      string
+	processes map[string]*trackedProcess
+	count     chan int
+}
+
+func NewNodeWorker() *NodeWorker {
+	nw := &NodeWorker{
+		name:      nodename,
+		processes: make(map[string]*trackedProcess),
+		count:     make(chan int),
+	}
+	go func() {
+		for i := 0; true; i++ {
+			nw.count <- i
+		}
+	}()
+	return nw
+}
+
+func (nw *NodeWorker) newProcessID() string {
+	return fmt.Sprintf("%v.%v", nw.name, <-nw.count)
+}
+
+type LaunchArgs struct {
+	Name string   `json:"name"`
+	Args []string `json:"args"`
+	Env  []string `json:"env"`
+	Cwd  string   `json:"cwd"`
+	CPUs int      `json:"ncpu"`
+}
+
+type LaunchReply struct {
+	ID string `json:"id"`
+}
+
+func (nw *NodeWorker) Launch(args *LaunchArgs, reply *LaunchReply) error {
+	cmd := exec.Command(args.Name, args.Args...)
+	cmd.Env = args.Env
+	cmd.Dir = args.Cwd
+	id := nw.newProcessID()
+	stdout, err := os.Create(id + "o")
+	if err != nil {
+		return err
+	}
+	defer stdout.Close()
+	stderr, err := os.Create(id + "e")
+	if err != nil {
+		return err
+	}
+	defer stderr.Close()
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	log.Printf("launched %s: %d %s", id, cmd.Process.Pid, args.Name)
+	nw.processes[id] = newTrackedProcess(cmd)
+	reply.ID = id
+	return nil
+}
+
+type QueryArgs struct {
+	ID string `json:"id"`
+}
+
+type QueryReply struct {
+	Exited     bool `json:"exited"`
+	ExitStatus int  `json:"exit_status"`
+}
+
+func (reply *QueryReply) setProcess(tp *trackedProcess) {
+	reply.Exited = tp.exited
+	reply.ExitStatus = tp.exitStatus
+}
+
+func (nw *NodeWorker) findProcess(id string) (*trackedProcess, error) {
+	tp, ok := nw.processes[id]
+	if !ok {
+		return nil, fmt.Errorf("No such process: %v", id)
+	}
+	return tp, nil
+}
+
+func (nw *NodeWorker) Query(args *QueryArgs, reply *QueryReply) error {
+	tp, err := nw.findProcess(args.ID)
+	if err != nil {
+		return err
+	}
+	reply.setProcess(tp)
+	return nil
+}
+
+func (nw *NodeWorker) Wait(args *QueryArgs, reply *QueryReply) error {
+	tp, err := nw.findProcess(args.ID)
+	if err != nil {
+		return err
+	}
+	<-tp.waitExited
+	reply.setProcess(tp)
+	return nil
+}
+
+type SignalArgs struct {
+	ID     string
+	Signal int
+}
+
+func (nw *NodeWorker) Signal(args *SignalArgs, reply *QueryReply) error {
+	tp, err := nw.findProcess(args.ID)
+	if err != nil {
+		return err
+	}
+	if !tp.exited {
+		err = syscall.Kill(tp.cmd.Process.Pid, syscall.Signal(args.Signal))
+		log.Printf(
+			"sent signal %v to %v, pid: %v",
+			args.Signal,
+			args.ID,
+			tp.cmd.Process.Pid,
+		)
+	}
+	reply.setProcess(tp)
+	return err
+}
+
+func jsonrpcDialTimeout(network, address string, timeout time.Duration) (*rpc.Client, error) {
+	conn, err := net.DialTimeout(network, address, timeout)
+	if err != nil {
+		return nil, err
+	}
+	return jsonrpc.NewClient(conn), nil
+}
+
+func rpcMain(rcvrs ...interface{}) {
+	rpc := rpc.NewServer()
+	for _, rcvr := range rcvrs {
+		err := rpc.Register(rcvr)
+		if err != nil {
+			log.Fatalf("fatal: error registering rpc: %v", err)
+		}
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("fatal: listen error: %v", err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalf("fatal: accept error: %v", err)
+		}
+		go rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+	}
+}
+
+func nodeWorkerMain() {
+	log.Println("node running at port", port)
+	rpcMain(NewNodeWorker())
+}
+
+type Node struct {
+	Name     string
+	CPUs     int
+	busyCPUs int
+	rpc      *rpc.Client
+	sshCmd   *exec.Cmd
+}
+
+func (node *Node) startWorker() {
+	abspath, err := filepath.Abs(os.Args[0])
+	if err != nil {
+		log.Fatalln("fatal:", err)
+	}
+	localname, err := os.Hostname()
+	if err != nil {
+		log.Fatalln("fatal:", err)
+	}
+	if node.Name != localname {
+		// maybe implement using ssh library sometime
+		sshCmd := exec.Command("ssh", "-t", "-t", node.Name, "--", abspath, "node")
+		sshCmd.Stdout = os.Stdout
+		sshCmd.Stderr = os.Stderr
+		err := sshCmd.Start()
+		if err != nil {
+			log.Fatalln("fatal:", err)
+		}
+		node.sshCmd = sshCmd
+	}
+	for rt := nodetimeout; true; rt -= time.Second {
+		sleepCh := time.After(time.Second)
+		var rpc *rpc.Client
+		rpc, err = jsonrpcDialTimeout(
+			"tcp",
+			fmt.Sprintf("%s:%d", node.Name, port),
+			rt,
+		)
+		if err == nil {
+			node.rpc = rpc
+			log.Println("started node worker at", node.Name)
+			return
+		}
+		<-sleepCh
+	}
+	log.Fatalln("fatal:", err)
+}
+
+func (node *Node) stopWorker() {
+	if node.sshCmd != nil {
+		node.sshCmd.Process.Signal(os.Interrupt)
+	}
+}
+
+type Master struct {
+	Nodes map[string]*Node
+}
+
+func (m *Master) Launch(args *LaunchArgs, reply *LaunchReply) error {
+	requestedCPUs := args.CPUs
+	if requestedCPUs < 1 {
+		requestedCPUs = 1
+	}
+	for _, node := range m.Nodes {
+		if requestedCPUs+node.busyCPUs < node.CPUs {
+			err := node.rpc.Call("NodeWorker.Launch", args, reply)
+			if err != nil {
+				node.busyCPUs += requestedCPUs
+				go func() {
+					var qreply QueryReply
+					node.rpc.Call(
+						"NodeWorker.Wait",
+						&QueryArgs{ID: reply.ID},
+						&qreply)
+					node.busyCPUs -= requestedCPUs
+				}()
+			}
+			return err
+		}
+	}
+	return errors.New("No idle CPU available")
+}
+
+func (m *Master) proxyQuery(serviceMethod string, id string, args interface{}, reply *QueryReply) error {
+	var hostname string
+	var subid int
+	err := split2(id, ".", &hostname, &subid)
+	if err != nil {
+		return err
+	}
+	node, ok := m.Nodes[hostname]
+	if !ok {
+		return fmt.Errorf("No such node: %q", hostname)
+	}
+	return node.rpc.Call("NodeWorker."+serviceMethod, args, reply)
+}
+
+func (m *Master) Query(args *QueryArgs, reply *QueryReply) error {
+	return m.proxyQuery("Query", args.ID, args, reply)
+}
+
+func (m *Master) Signal(args *SignalArgs, reply *QueryReply) error {
+	return m.proxyQuery("Signal", args.ID, args, reply)
+}
+
+func (m *Master) Wait(args *QueryArgs, reply *QueryReply) error {
+	return m.proxyQuery("Wait", args.ID, args, reply)
+}
+
+func getNodesFromMachineFile(filename string) map[string]*Node {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln("fatal:", err)
+	}
+	defer file.Close()
+	result := make(map[string]*Node)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		parts := strings.SplitN(scanner.Text(), ":", 2)
+		if len(parts) != 2 {
+			log.Fatalf("bad word: %q", scanner.Text())
+		}
+		hostname := parts[0]
+		var cpucount int
+		_, err := fmt.Sscan(parts[1], &cpucount)
+		if err != nil {
+			log.Fatalf("bad word (cpus): %q", scanner.Text())
+		}
+		result[hostname] = &Node{
+			Name: hostname,
+			CPUs: cpucount,
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Error reading input: %v", err)
+	}
+	return result
+}
+
+func masterMain() {
+	var master Master
+	master.Nodes = getNodesFromMachineFile("machinefile")
+	go rpcMain(&master, NewNodeWorker())
+	for _, node := range master.Nodes {
+		node.startWorker()
+	}
+	log.Println("master running at port", port)
+
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, os.Interrupt)
+	<-sigchan
+	fmt.Println("received Ctrl+C, quitting")
+	for _, node := range master.Nodes {
+		node.stopWorker()
+	}
+}
+
+func main() {
+	log.SetFlags(log.Ltime | log.Lshortfile)
+	log.SetPrefix(nodename + " ")
+	flag.Parse()
+	if len(os.Args) != 2 {
+		log.Fatalf("usage: %v {master,node}", os.Args[0])
+	}
+	command := os.Args[1]
+	if command == "master" {
+		masterMain()
+	} else if command == "node" {
+		nodeWorkerMain()
+	} else {
+		log.Fatal("unknown command: ", command)
+	}
+}
---
+++ afqc.py
@@ -0,0 +1,143 @@
+'''
+client for afq
+'''
+
+import threading
+import collections
+import socket
+import os
+import json
+import weakref
+
+
+class Receiver(object):
+    '''
+    event wrapper for receiving asynchronous json rpc response
+    '''
+    __slots__ = ('event', 'value', '__weakref__')
+    def __init__(self):
+        self.event = threading.Event()
+
+
+class RemoteException(Exception):
+    '''
+    Error raised by the remote call
+    '''
+    pass
+
+
+class RPCClinet(object):
+    '''low level jsonrpc client'''
+    def __init__(self, host, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((host, port))
+        self.count = 0
+        self.count_lock = threading.Lock()
+        self.receivers = weakref.WeakValueDictionary()
+        thread = threading.Thread(target=self._readloop)
+        thread.setDaemon(True)
+        thread.start()
+
+    def _get_id(self):
+        with self.count_lock:
+            self.count += 1
+            return self.count
+
+    def _send_command(self, command, kwargs):
+        receiver = Receiver()
+        id = self._get_id()
+        self.receivers[id] = receiver
+        self.socket.sendall(
+            json.dumps({
+                'method': command,
+                'params': [kwargs],
+                'id': id
+            }) +
+            '\n'
+        )
+        receiver.event.wait()
+        return receiver.value
+
+    def call(self, method, **kwargs):
+        '''
+        call the method on the RPC server
+        return
+        '''
+        result = self._send_command(method, kwargs)
+        if result['error'] is not None:
+            raise RemoteException(result['error'])
+        else:
+            return result['result']
+
+    def _readloop(self):
+        file = self.socket.makefile('r')
+        for line in file:
+            recvobj = json.loads(line)
+            receiver = self.receivers[recvobj['id']]
+            receiver.value = recvobj
+            receiver.event.set()
+
+
+ProcessStatus = collections.namedtuple(
+    'ProcessStatus',
+    ('exited', 'exit_status'))
+
+
+class Master(object):
+    '''
+    Client to talk to afq
+    '''
+    def __init__(self, host, port):
+        self.rpc = RPCClinet(host, port)
+
+    def query(self, id):
+        '''
+        query the process by given id
+        returns a ProcessStatus
+        '''
+        result = self.rpc.call('Master.Query', id=id)
+        return ProcessStatus(**result)
+
+    def launch(self, *args, **kwargs):
+        '''
+        launch a process
+
+        *args: the command to run
+        cwd: working directory.
+             defaults to the current working directory
+        env: environment variables, should be a dict.
+             defaults to the current environment variables
+        ncpu: number of reserved cpus, defaults to 1
+
+        returns the id to refer to the proecss
+        '''
+        cwd = kwargs.get('cwd', os.getcwd())
+        env = kwargs.get('env', os.environ)
+        ncpu = kwargs.get('ncpu', 1)
+        env = ['%s=%s' % (k, v) for (k, v) in env.items()]
+        result = self.rpc.call(
+            'Master.Launch',
+            name=args[0],
+            args=args[1:],
+            cwd=cwd,
+            env=env,
+            ncpu=ncpu)
+        return result['id']
+
+    def signal(self, id, signal):
+        '''
+        send a signal to the process
+        returns a ProcessStatus
+
+        Note: The process may not terminate immediately.
+        '''
+        result = self.rpc.call('Master.Signal', id=id, signal=signal)
+        return ProcessStatus(**result)
+
+    def wait(self, id):
+        '''
+        wait for the process with the id to exit.
+        returns a ProcessStatus
+        '''
+        result = self.rpc.call('Master.Wait', id=id)
+        return ProcessStatus(**result)
